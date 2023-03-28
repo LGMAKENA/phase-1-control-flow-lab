9aacb85dc4ea42abc5964db5e49427cf7bfb310e
lgmakena <linetmakenag@gmail.com>
2023-03-28 15:30:00 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if(distance < 400)
+  {
+    return"This one is on me!";
+  }else if(distance>2500)
+  {
+    return "No can do."
+  }else if(distance>2000)
+  {
+return "I will gladly take your thirty bucks."
+  }
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  
   // Write your code here!
+  return (city=="NYC") ? "Ok, sounds good." :  "No go."
+
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip)
+  {
+    case "generous":
+      return ("Thank you so much.")
+      case "not as generous":
+      return ("Thank you.")
+ default:
+
+       return ("Bye.")
+  }
+
   // Write your code here!
+
 }
+
+
+console.log(scuberGreetingForFeet(2600));
---
+++ practice.js
@@ -0,0 +1,69 @@
+// if (green = "cross the road")
+
+
+// if (green !== "cross the road")
+// {
+// expressions
+// }
+
+
+// // examples of conditions
+
+// 2 === 4;
+// 2 !== 4;
+// green === "cross the road";
+// 2 <= 4;
+// 4 >= 2;
+
+
+let num1= 76;
+
+if (num1 <= 10)
+{
+    console.log("true");
+} else 
+{
+    console.log("false");
+}
+
+let num2= 3;
+
+
+if (num2 === 1)
+{
+    console.log("ONE");
+} else if (num2 === 2)
+{
+    console.log("TWO");
+}else if (num2 === 3)
+[
+    console.log("THREE")
+] 
+
+
+const num3= 1;
+
+// switch uses cases instead of 
+switch (num3) {
+    case 1: 
+        console.log("one")
+        break;
+
+        case 2:
+            console.log("two")
+            break;
+        case 3:
+            console.log("three")
+            break
+    default:
+        console.log("unknown")
+        break;
+}
+
+
+
+const score = 50;
+
+var = condition ? true : false;
+
+
